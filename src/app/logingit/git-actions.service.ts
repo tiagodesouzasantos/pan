6aee5404b6150c0cde1fe02dd66833d0a939b243
Tiago <tiago.souza.santos1989@gmail.com>
2019-04-07 14:32:50 -0300

    Publicacao

--- src/app/logingit/git-actions.service.ts
+++ src/app/logingit/git-actions.service.ts
@@ -12,7 +12,7 @@ export class GitActionsService {
     private session: SessionService) { }
 
   valideSession(code:string){
-    return this.httpClient.post('/api/login/oauth/access_token', {
+    return this.httpClient.post('https://github.com/login/oauth/access_token', {
           client_id: 'cc5d6a52910648e97389',
           client_secret: '1163f8081f7a567b5cd264b2b42394236a293323',
           code: code
